a4f9c01cf6c606186d7caa0461eca66361dad2ee
GCapaross <gvds@ua.pt>
2025-03-09 20:16:26 +0000

    Logs page but with a twist Done differently from figma

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import LoginPage from "./pages/Login/LoginPage";
 import TestPage from "./pages/test/TestPage";
 import UploadVideosPage from "./pages/upload_videos/UploadVideosPage";
 import CamerasPage from "./pages/cameras_page_1/CamerasPage";
+import ReportsPage from "./pages/report/ReportsPage";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         {/* <Route path="/" element={<LoginPage />} /> */}
         {/* <Route path="/test" element={<TestPage />} /> */}
         {/* <Route path="/" element={<UploadVideosPage />} /> */}
-        <Route path="/" element={<CamerasPage />} />
+        {/* <Route path="/" element={<CamerasPage />} /> */}
+        <Route path="/" element={<ReportsPage />} />
       </Routes>
     </Router>
   );
