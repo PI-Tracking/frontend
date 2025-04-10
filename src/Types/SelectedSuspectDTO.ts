e04b62b129235621ae99fbe172fa15ed67973881
Affapple <joaopmgaspar@ua.pt>
2025-04-10 12:24:19 +0100

    Added analysis controller
    dayum its confusing af

---
+++ src/Types/SelectedSuspectDTO.ts
@@ -0,0 +1,7 @@
+export default interface SelectedSuspectDTO {
+  videoId: string;
+  frame: number; // timestamp
+  //coordinates
+  x: number;
+  y: number;
+}
---
+++ src/api/analysis.ts
@@ -0,0 +1,62 @@
+import { UUID } from "@Types/Base";
+import apiClient from "./api";
+import { AxiosResponse } from "axios";
+import SelectedSuspectDTO from "@Types/SelectedSuspectDTO";
+
+const baseEndpoint = "/analysis";
+
+async function requestNewAnalysis(
+  camerasId: string[],
+  files: string[]
+): Promise<AxiosResponse<string>> {
+  if (camerasId.length != files.length) {
+    Promise.reject({ error: "Number of cameras and files must be the same!" });
+  }
+  /**
+   * GET /analysis/live
+   * Return:
+   *    SUCCESS
+   *     { data: string[]; status: OK }
+   *     string[] is an array of UUID to be used in MinIO file upload
+   *
+   *    FAILURE
+   *     status: BAD_REQUEST | UNAUTHORIZED | FORBIDDEN
+   */
+
+  const endpoint = `${baseEndpoint}/live/camerasId=${camerasId.join(",")}`;
+  return apiClient.get(endpoint);
+}
+
+async function requestReanalysis(
+  reportId: UUID,
+  selectedSuspect?: SelectedSuspectDTO
+): Promise<AxiosResponse<string>> {
+  /**
+   * POST /analysis/{reportId}
+   * Return:
+   *    SUCCESS
+   *     { data: string[]; status: OK }
+   *     string[] is an array of UUID of the new analysis (idk why its a fkn list)
+   *
+   *    FAILURE
+   *     status: BAD_REQUEST | UNAUTHORIZED | FORBIDDEN
+   */
+  const endpoint = `${baseEndpoint}/${reportId}`;
+  return apiClient.post(endpoint, selectedSuspect);
+}
+
+async function stopAnalysis(analysisId: UUID): Promise<AxiosResponse> {
+  /**
+   * POST /analysis/live/{analysisId}
+   * Return:
+   *    SUCCESS
+   *     { status: OK }
+   *
+   *    FAILURE
+   *     status: BAD_REQUEST | UNAUTHORIZED | FORBIDDEN
+   */
+  const endpoint = `${baseEndpoint}/live/${analysisId}`;
+  return apiClient.post(endpoint);
+}
+
+export { requestNewAnalysis, requestReanalysis, stopAnalysis };
