5f9d403909e400b18648750a7f00473272c24286
BirrasOBacano <birras13@sapo.pt>
2025-05-07 22:21:44 +0100

    add report page refactor with link to map

--- src/pages/Reports/index.tsx
+++ src/pages/Reports/index.tsx
@@ -1,84 +1,58 @@
-import { useState } from "react";
 import CameraMenuOptions from "@components/CameraMenuOptions";
 import Navbar from "@components/Navbar";
 import "./ReportsPage.css";
+import useReport from "@hooks/useReportStore";
+import { useNavigate } from "react-router-dom";
+import { Report } from "@Types/Report";
 
 function ReportsPage() {
-  const [activeTab, setActiveTab] = useState("weapon");
+  const { setReport } = useReport();
+  const navigate = useNavigate();
 
-  const weaponReports = [
+  const reports = [
     {
-      id: 132,
-      location: "Rua do Mac",
-      reviewers: [
-        { id: 1, initial: "A", color: "grey" },
-        { id: 2, initial: "A", color: "grey" },
-      ],
-    },
-    {
-      id: 133,
-      location: "Rua do Mac",
-      reviewers: [
-        { id: 1, initial: "A", color: "grey" },
-        { id: 2, initial: "A", color: "grey" },
+      id: "132",
+      name: "Rua do Mac",
+      creator: "John Doe",
+      createdAt: "2023-10-01",
+      uploads: [
+        { id: 1, name: "Upload 1" },
+        { id: 2, name: "Upload 2" },
       ],
     },
   ];
+
+  const selectReport = (report: Report) => {
+    if (!report) return;
+    console.log("Selected report ID:", report.id);
+    setReport(report);
+    navigate(`/map-tracking`);
+  };
 
   return (
     <div className="container">
       <Navbar />
       <section className="reports-section">
         <h1 className="reports-title">Reports</h1>
-
-        <div className="tabs">
-          <button
-            className={`tab ${activeTab === "weapon" ? "active" : ""}`}
-            onClick={() => setActiveTab("weapon")}
-          >
-            Weapon Detections
-          </button>
-          <button
-            className={`tab ${activeTab === "other" ? "active" : ""}`}
-            onClick={() => setActiveTab("other")}
-          >
-            Other reports
-          </button>
-        </div>
-
         <div className="reports-content">
-          {activeTab === "weapon" && (
-            <div className="reports-list">
-              {weaponReports.map((report) => (
-                <div key={report.id} className="report-card">
-                  <div className="report-info">
-                    <div className="report-id">{report.id}</div>
-                    <div className="report-location">{report.location}</div>
-                  </div>
-                  <div className="report-reviewers">
-                    <div className="reviewer-label">Reviewed by</div>
-                    <div className="reviewer-avatars">
-                      {report.reviewers.map((reviewer) => (
-                        <div
-                          key={reviewer.id}
-                          className="reviewer-avatar"
-                          style={{ backgroundColor: reviewer.color }}
-                        >
-                          {reviewer.initial}
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          )}
-
-          {activeTab === "other" && (
-            <div className="reports-list">
-              <p>Other repotrs will be displayed here</p>
-            </div>
-          )}
+          <div className="reports-list">
+            {reports.map((report) => (
+              <div key={report.id} className="report-card">
+                <h2 className="report-name">{report.name}</h2>
+                <p className="report-creator">Created by: {report.creator}</p>
+                <p className="report-date">Created at: {report.createdAt}</p>
+                <p className="uploads-size">
+                  Analysed videos: {report.uploads.length}
+                </p>
+                <button
+                  className="view-report-button"
+                  onClick={() => selectReport(report)}
+                >
+                  View Movements
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </section>
 
