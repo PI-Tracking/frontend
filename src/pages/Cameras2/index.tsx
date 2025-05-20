fc4098412a2a3897998aba6e42b1d0a25b00c12b
David Amorim <davidfilipeamorim@gmail.com>
2025-05-20 19:15:56 +0100

    Fix bug in new analysis request

--- src/pages/Cameras2/index.tsx
+++ src/pages/Cameras2/index.tsx
@@ -6,12 +6,21 @@ import CameraMenuOptions from "@components/CameraMenuOptions";
 import "./CamerasPageMap.css";
 import { getAllCameras } from "@api/camera";
 import { Camera } from "@Types/Camera";
+import VideoViewer from "./components/VideoViewer";
+import { requestNewAnalysis } from "@api/analysis";
 
 const COIMBRA: [number, number] = [40.202852, -8.410192];
+
+type Frames = {
+  [camera: string]: string;
+};
+
 function CamerasPageMap() {
   const [cameras, setCameras] = useState<Camera[]>([]);
   const [showMap, setShowMap] = useState<boolean>(false);
   const [selectedCameras, setSelectedCameras] = useState<Camera[]>([]);
+
+  const [frames, setFrames] = useState<Frames>({});
 
   useEffect(() => {
     const fetchCameras = async () => {
@@ -36,6 +45,28 @@ function CamerasPageMap() {
       setSelectedCameras([...selectedCameras, camera]);
     }
     setShowMap(false);
+  };
+
+  const handleStartLiveAnalysis = async () => {
+    await requestNewAnalysis(selectedCameras.map((c) => c.id));
+
+    const websocket = new WebSocket(
+      `ws://localhost:8000/api/v1/live/ws/${Math.floor(Math.random() * 1_000_000)}`
+    );
+
+    websocket.onopen = () => {
+      console.log("Live WS Opened");
+    };
+
+    websocket.onmessage = (event) => {
+      const json = JSON.parse(event.data);
+
+      const cameraId = json.camera_id;
+
+      setFrames((prev) => {
+        return { ...prev, [cameraId]: json.frame };
+      });
+    };
   };
 
   return (
@@ -78,15 +109,11 @@ function CamerasPageMap() {
         </section>
       )}
 
-      {selectedCameras.length > 0 && (
-        <section className="camera-feed">
-          {selectedCameras.map((camera) => (
-            <div key={camera.id} className="camera-view">
-              Camera {camera.name}
-            </div>
-          ))}
-        </section>
-      )}
+      {selectedCameras.map((camera) => (
+        <VideoViewer key={camera.id} frame={frames[camera.id]} />
+      ))}
+
+      <button onClick={handleStartLiveAnalysis}>Start Live Analysis</button>
 
       <div className="menu-options">
         <CameraMenuOptions />
