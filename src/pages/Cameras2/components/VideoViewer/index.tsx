1966d4403d7caed94401e8bab5f1c65564e6703e
David Amorim <davidfilipeamorim@gmail.com>
2025-05-27 11:35:55 +0100

    Add waiting for frames message along with the corresponding cameraid

--- src/pages/Cameras2/components/VideoViewer/index.tsx
+++ src/pages/Cameras2/components/VideoViewer/index.tsx
@@ -1,16 +1,26 @@
 import "./VideoViewer.css";
 
 type Props = {
-  frame: string;
+  frame?: string;
+  cameraId: string;
 };
 
-function VideoViewer({ frame }: Props) {
+function VideoViewer({ frame, cameraId }: Props) {
   return (
-    <img
-      className="camera-view"
-      src={`data:image/jpeg;base64,${frame}`}
-      alt="Video Frame"
-    />
+    <>
+      {frame ? (
+        <img
+          className="camera-view"
+          src={`data:image/jpeg;base64,${frame}`}
+          alt="Video Frame"
+        />
+      ) : (
+        <div className="camera-view">
+          <p>Waiting for frames...</p>
+          <p>Camera ID: {cameraId}</p>
+        </div>
+      )}
+    </>
   );
 }
 
